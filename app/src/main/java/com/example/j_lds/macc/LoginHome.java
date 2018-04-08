f0ea59924c1fbb6349fd4d984200f9ffbce9a133
Jeanlaurent <jl.duzant@hotmail.com>
2018-04-08 14:59:39 -0400

    *Créer LoginHome Activité et layout

---
+++ app/src/main/java/com/example/j_lds/macc/LoginHome.java
@@ -0,0 +1,13 @@
+package com.example.j_lds.macc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_home);
+    }
+}

06a2fd226de45a7d69024d8b27f1bac80fe8384e
Jeanlaurent <jl.duzant@hotmail.com>
2018-04-08 15:29:10 -0400

    *[Version 0.6] - private class Login *Nouveau fichier "ConnectionClass"    -function CONN V1

---
+++ app/src/main/java/com/example/j_lds/macc/ConnectionClass.java
@@ -0,0 +1,46 @@
+package com.example.j_lds.macc;
+
+
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//hostname local 192.168.13.91
+//hostname distant 93.121.229.118
+// db user MACC
+//user pi
+//passDB Simconolat
+
+public class ConnectionClass{
+    String classs = "com.mysql.jdbc.Driver";
+
+    String url = "jdbc:mysql://93.121.229.118/MACC";
+    String userName = "pi";
+    String password = "Simconolat";
+
+    public Connection CONN() {
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+            //trying to connect to the database
+            Class.forName(classs);
+            conn = DriverManager.getConnection(url, userName, password);
+            conn = DriverManager.getConnection(ConnURL);
+
+        } catch (SQLException se) {
+            Log.e("ERRO", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO", e.getMessage());
+        }
+        return conn;
+    }
+}
--- app/src/main/java/com/example/j_lds/macc/LoginHome.java
+++ app/src/main/java/com/example/j_lds/macc/LoginHome.java
@@ -1,13 +1,110 @@
 package com.example.j_lds.macc;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class LoginHome extends AppCompatActivity {
+
+    EditText user,pass;
+    Button login;
+    ProgressDialog progressDialog;
+    ConnectionClass connectionClass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_home);
+
+        getSupportActionBar().hide(); //Hides MyFirsApp
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        user = (EditText) findViewById(R.id.User);
+        pass= (EditText) findViewById(R.id.pass);
+        login= (Button) findViewById(R.id.login);
+
+        connectionClass = new ConnectionClass();
+
+        progressDialog=new ProgressDialog(this);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login login=new Login();
+                login.execute();
+            }
+        });
+    }
+
+    private class Login extends AsyncTask<String,String,String>
+    {
+        String userStr = user.getText().toString();
+        String passStr =pass.getText().toString();
+        String z="";
+        boolean isSuccess=false;
+
+        String userDB, passDB;
+
+
+        @Override
+        protected void onPreExecute() {
+
+            progressDialog.setMessage("Loading...");
+            progressDialog.show();
+
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            if (userStr.trim().equals("") || passStr.trim().equals(""))
+                z = "Please enter all fields....";
+            else {
+                try {
+                    Connection con = connectionClass.CONN();
+                    if (con == null) {
+                        z = "Please check your internet connection";
+                    } else {
+
+                        String query = " select * from PROFESSEUR where NOM='" + userStr + "' and MDP = '" + passStr + "'";
+                        Statement stmt = con.createStatement();
+                        //stmt.executeUpdate(query);
+                        ResultSet rs = stmt.executeQuery(query);
+
+                        while (rs.next()) {
+                            userDB = rs.getString(2);
+                            passDB = rs.getString(5);
+
+                            if (userDB.equals(userStr) && passDB.equals(passStr)) {
+                                isSuccess = true;
+                                z = "Login successfull";
+                            } else {
+                                isSuccess = false;
+                            }
+                        }
+                    }
+                } catch (Exception ex) {
+                    isSuccess = false;
+                    z = "Exceptions" + ex;
+                }
+            }
+            return z;
+        }
+        @Override
+        protected void onPostExecute(String s) {
+
+        }
     }
 }

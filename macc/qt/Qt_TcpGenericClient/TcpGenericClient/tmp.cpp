95c7962dd4db9458bfc5b1f1a8b30ba11a65709b
Pascal BARREAU <4.barreau.pascal@gmail.com>
2018-04-15 08:56:53 -0500

    Rajout de l'executable windows + fichier tmp.cpp

---
+++ macc/qt/Qt_TcpGenericClient/TcpGenericClient/tmp.cpp
@@ -0,0 +1,11 @@
+#if 0
+http://www.jrsoftware.org/isinfo.php
+
+https://stackoverflow.com/questions/35970727/use-of-variables-like-builddir-in-qtcreator-kit-settings-in-qt5
+Tools (menu) --> Options --> Environment (tab) --> External Tools
+Click "Update Translations..."
+Click inside "Working Directory.." and you should see a "AB->" icon in colour to the right.
+Click the icon for your list of vars.
+
+http://qtfr.dergen.fr/discussion/18027/deployer-un-programme-qt5-sur-windows-avec-windeployqt
+#endif

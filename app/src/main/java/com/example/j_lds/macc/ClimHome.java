d85b509e4b48ad55373d0bc6d71762a612a94281
Jeanlaurent <jl.duzant@hotmail.com>
2018-05-07 15:23:38 -0400

    [App Version 6]
    *ClimHome.java
      -can access ESP Salle-13
      -send sockets (minus and up)

--- app/src/main/java/com/example/j_lds/macc/ClimHome.java
+++ app/src/main/java/com/example/j_lds/macc/ClimHome.java
@@ -9,6 +9,7 @@ import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
@@ -21,36 +22,35 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class ClimHome extends AppCompatActivity {
 
     int on_Off = 0 ;
-    String unite = "° C";
-    Button button_ON_OFF;
-    Button button_PLUS;
-    Button button_MINUS;
-    Button button_MODE;
-    Button button_GRAPH;
-    Button button_BACK;
-
-    String message = "";
+    private String unite = "° C";
+    private Button button_ON_OFF;
+    private Button button_PLUS;
+    private Button button_MINUS;
+    private Button button_GRAPH;
+    private Button button_BACK;
 
     TextView nameText, nameSalle;
     ProgressDialog progressDialog;
     ConnectionClass connectionClass;
 
-    private static Socket s;
-    private static PrintWriter pw;
+    private Socket s;
+    private PrintWriter pw;
     //hostname local 192.168.13.91 RPI RESEAUX V13
     //hostname distant 93.121.229.118 CHEZ MATHIAS
     //hostname local 192.168.137.127
-    private static String host = ""; //192.168.1.21
-    private static String hostTest = "192.168.137.127";
-    private static int port = 1060;
-    private static int portTest = 1060;
-    private static String dataPass = "";
+    private String hostTest= "172.20.10.2"; //192.168.1.21
+    private String host = "192.168.4.1";
+    private int port = 1060;
+    private int portTest = 8024;
 
-    private String salle,clim;
+    private int intTempValue;
+    private int startTemp = 26;
+    private String clim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,18 +63,17 @@ public class ClimHome extends AppCompatActivity {
 
         String name = getIntent().getStringExtra("user");
         nameText = (TextView) findViewById(R.id.textView_name);
-        nameText.setText("Hello "+name);
+        nameText.setText("Hello\n"+name);
         ///////////////////////////////////////////////////////////////////////////////////////////////
         nameSalle = (TextView)findViewById(R.id.textView_salle);
-        salle = e4Csg1MACC_getWifiSSID();
-        nameSalle.setText("Salle: "+salle);
+        String salle = e4Csg1MACC_getWifiSSID();
+        nameSalle.setText("Salle:\n"+ salle);
         //pass from loginHome the class(la sale) number where the user is suppose to be.
 
         //declare my buttons........................................................................
         button_ON_OFF = (Button) findViewById(R.id.button_on_off);
         button_PLUS = (Button) findViewById(R.id.button_Plus);
         button_MINUS = (Button) findViewById(R.id.button_minus);
-        button_MODE = (Button) findViewById(R.id.button_mode);
         button_GRAPH = (Button) findViewById(R.id.button_ok);
         button_BACK = (Button) findViewById(R.id.button_back);
 
@@ -97,12 +96,6 @@ public class ClimHome extends AppCompatActivity {
                 e4Csg1Macc_minus();
             }
         });
-        button_MODE.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                e4Csg1Macc_mode();
-            }
-        });
         button_GRAPH.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -119,7 +112,9 @@ public class ClimHome extends AppCompatActivity {
         progressDialog=new ProgressDialog(this);
         connectionClass = new ConnectionClass();
 
-        
+        intTempValue = startTemp;
+        showClim();
+
     }
 
     /*get the "Salle" name by the wifi ssid
@@ -127,127 +122,148 @@ public class ClimHome extends AppCompatActivity {
      */
     public String e4Csg1MACC_getWifiSSID(){
         WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        WifiInfo info = wifiManager.getConnectionInfo ();
+        WifiInfo info = wifiManager.getConnectionInfo();
         String ssid  = info.getSSID();
         return ssid;
     }
 
-    private void e4Csg1MACC_sendSocket(){
-
-        try {
-            s = new Socket(hostTest, portTest);
-            pw = new PrintWriter(s.getOutputStream());
-            pw.write(dataPass);
-            pw.flush();
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
+    //this methode is for the alternative button on off.............................................
     protected void e4Csg1Macc_on_Off() {
-        Button b = (Button) findViewById(R.id.button_on_off);
         TextView tv = (TextView) findViewById(R.id.textView_tempView);
-        int color;
-
-        if (on_Off == 0) {
-            //button on.............................................................................
-            on_Off = 1;
-            b.setText("Off"); // Arrêt
-            color = Color.parseColor("#ff99cc00");
-            b.setBackgroundColor(color);
-
-            dataPass = "A";
-            E4sendSocket ss = new E4sendSocket();
-            ss.execute();
-            tv.setText("26" + unite);
-
-        } else {
-            //button off............................................................................
-            on_Off = 0;
-            b.setText("On"); //Marche
-            color = Color.parseColor("#ffcc0000");
-            b.setBackgroundColor(color);
-
-            dataPass = "B";
-            E4sendSocket ss = new E4sendSocket();
-            ss.execute();
-            tv.setText("0" + unite);
-
+        if (clim.equals("Please press to select a AC")){
+            Toast.makeText(getBaseContext(), "Please select a AC !!!", Toast.LENGTH_LONG).show();
+        }else{
+            if (on_Off == 0) {
+                //button on.............................................................................
+                if (intTempValue == startTemp){
+                    on_Off = 1;
+                    button_ON_OFF.setText("Off"); // Arrêt
+                    button_ON_OFF.setBackgroundColor(Color.parseColor("#ff99cc00"));
+                    E4sendSocket ss = new E4sendSocket();
+                    ss.execute();
+                    tv.setText("" + startTemp);
+                }else{
+                    on_Off = 1;
+                    button_ON_OFF.setText("Off"); // Arrêt
+                    button_ON_OFF.setBackgroundColor(Color.parseColor("#ff99cc00"));
+                    E4sendSocket ss = new E4sendSocket();
+                    ss.execute();
+                    tv.setText("" + intTempValue);
+                }
+            } else {
+                //button off............................................................................
+                on_Off = 0;
+                button_ON_OFF.setText("On"); //Marche
+                button_ON_OFF.setBackgroundColor(Color.parseColor("#ffcc0000"));
+                tv.setText(" ");
+            }
         }
     }
 
+    //this methode is for up button.................................................................
     protected void e4Csg1Macc_plus(){
         //button up.................................................................................
-        dataPass = "C";
-        E4sendSocket ss = new E4sendSocket();
-        ss.execute();
+        TextView tv = (TextView) findViewById(R.id.textView_tempView);
+        String stringTempValue = tv.getText().toString();
+        if (clim.equals("Please press to select a AC")){
+            Toast.makeText(getBaseContext(), "Please select a AC !!!", Toast.LENGTH_SHORT).show();
+        }else {
+            if (on_Off == 1) {
+                if (intTempValue < 30) {
+                    intTempValue = Integer.parseInt(stringTempValue);   //convert int to string
+                    intTempValue++;
+                    E4sendSocket ss = new E4sendSocket();
+                    ss.execute();
+
+                    tv.setText("" + intTempValue);
+                    button_MINUS.setBackgroundColor(Color.parseColor("#ffffff"));
+                }
+            } else {
+                Toast.makeText(getBaseContext(), "AC is off", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
+    //this methode is for down button...............................................................
     protected void e4Csg1Macc_minus(){
         //button down...............................................................................
-        dataPass = "D";
-        E4sendSocket ss = new E4sendSocket();
-        ss.execute();
+        TextView tv = (TextView) findViewById(R.id.textView_tempView);
+        String stringTempValue = tv.getText().toString();
+
+        if (clim.equals("Please press to select a AC")){
+            Toast.makeText(getBaseContext(), "Please select a AC !!!", Toast.LENGTH_SHORT).show();
+        }else {
+            if (on_Off == 1) {
+                if (intTempValue > 16) {
+                    intTempValue = Integer.parseInt(stringTempValue);
+                    intTempValue--;
+                    E4sendSocket ss = new E4sendSocket();
+                    ss.execute();
+
+                    tv.setText("" + intTempValue);
+                    button_MINUS.setBackgroundColor(Color.parseColor("#ffffff"));
+                }
+            } else {
+                Toast.makeText(getBaseContext(), "AC is off", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
-    protected String e4Csg1Macc_mode(){
-        //button e4Csg1Macc_mode...............................................................................
-        String z = "This button is not yet available";
-        return z;
-/*
-        dataPass = "D";
-        E4sendSocket mt = new E4sendSocket();
-        mt.execute();
-        */
-    }
-
+    //this methode is for graph button to access the graph temperature..............................
     protected void e4Csg1Macc_tempGraph(){
         //open the graph screen.....................................................................
         Intent intent = new Intent(this, TemperatureGraph.class);
         startActivity(intent);
     }
 
+    //this methode is for exit button to go back to the identification screen.......................
     protected void e4Csg1Macc_exitToLogin(){
         //back......................................................................................
-        try {
-            pw.close();
-            s.close();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
         Intent intent = new Intent(this, LoginHome.class);
         startActivity(intent);
     }
 
+    //this sub class is serve to execute e4Csg1MACC_sendSocket() in the background..................
     private class E4sendSocket extends AsyncTask<Void, Void, Void>
     {
-
         @Override
         protected Void doInBackground(Void... voids) {
-            e4Csg1MACC_sendSocket();
+            try {
+                s = new Socket(host, port);
+                pw = new PrintWriter(s.getOutputStream());
+                pw.write(intTempValue);
+                pw.flush();
+                pw.close();
+                s.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             return null;
         }
     }
 
-    //find climatiseur..............................................................................
+    //this methode is ...
     private void showClim(){
 
         //get the spinner from the xml.
         Spinner spinner = findViewById(R.id.spinner_clim);
+
         //create a list of items for the spinner.
-        String[] languages = {" ", "English","Français",};
+        String[] languages = {"Please press to select a AC", "Clim","Clim1","Clim3","All AC's"};
+        ArrayList<String> language = new ArrayList<String>();
+
         //create an adapter to describe how the items are displayed, adapters are used in several places in android.
         //There are multiple variations of this, but this is the basic variant.
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, languages);
+
         //set the spinners adapter to the previously created one.
         spinner.setAdapter(adapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getBaseContext(), "Text : " +parent.getItemAtPosition(position), Toast.LENGTH_LONG).show();
+                Toast.makeText(getBaseContext(), "AC selected : " +parent.getItemAtPosition(position), Toast.LENGTH_LONG).show();
                 clim = parent.getItemAtPosition(position).toString();
             }
 
@@ -258,5 +274,4 @@ public class ClimHome extends AppCompatActivity {
 
         });
     }
-
 }
